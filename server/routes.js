e2c30b550f89e447315c71846bbcde535429c697
Taylor Shephard <lasha.tay@gmail.com>
2018-06-14 22:16:38 -0500

    commented out subrscription post request

--- server/routes.js
+++ server/routes.js
@@ -65,7 +65,6 @@ router.get('/api/getCurrentUser', (req, res) => {
   res.send(req.user.firstName);
 });
 
-<<<<<<< HEAD
 // router.post('/api/saveSubscription', (req, res) => {
 //   const isValidSaveRequest = (req, res) => {
 //     if (!req.body || !req.body.endpoint) {
@@ -102,55 +101,6 @@ router.get('/api/getCurrentUser', (req, res) => {
 //     isValidSaveRequest(req, res);
   
 // });
-=======
-router.post('/api/saveSubscription', (req, res) => {
-  const isValidSaveRequest = (req, res) => {
-    if (!req.body || !req.body.endpoint) {
-      res.status(400);
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({
-        error: {
-          id: 'no-endpoint',
-          message: 'Subscription must have an endpoint.',
-        },
-      }));
-      return false;
-    }
-    return true;
-  };
-
-  if (isValidSaveRequest(req, res)) {
-    return controller.saveSubscription(req.body.subscription, req.user.googleId)
-      .then((newSubscription) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({ data: { success: true } }));
-      })
-      .catch((err) => {
-        res.status(500);
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({
-          error: {
-            id: 'unable-to-save-subscription',
-            message: 'The subscription was received but we were unable to save it to our database.',
-          },
-        }));
-      });
-  }
-  isValidSaveRequest(req, res);
-
-});
-
-router.get('/api/upcomingEvents', (req, res) => {
-  const currentUserId = req.user.googleId;
-  // const currentUserId = req.query.googleId; // for testing in Postman
-  controller.fetchUpcomingEvents(currentUserId, (error, events) => {
-    if (error) {
-      console.error(error);
-    } else {
-      const chronological = events.sort((a, b) => {
-        const dateA = moment(a.date).unix();
-        const dateB = moment(b.date).unix();
->>>>>>> 3fb30f7c465299ecc12e45aea8dff585864bd29f
 
 // router.get('/api/upcomingEvents', (req, res) => {
 //   const currentUserId = req.user.googleId;
