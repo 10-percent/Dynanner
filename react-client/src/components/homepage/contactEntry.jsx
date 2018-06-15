f5c8d0dae4e481917a96dd4de368942037820c1a
Taylor Shephard <lasha.tay@gmail.com>
2018-06-15 16:09:28 -0500

    user can send their own message to contacts

--- react-client/src/components/homepage/contactEntry.jsx
+++ react-client/src/components/homepage/contactEntry.jsx
@@ -1,38 +1,48 @@
 import React from 'react';
 import Axios from 'axios';
 import { Link } from 'react-router-dom';
+import Popup from 'reactjs-popup';
 
-const ContactEntry = ({ contact }) => {
-  return (
-    <div
-      className="list-group-item list-group-item-action flex-column align-items-start past-event-entry"
-      to={{
-        pathname: "/sendSMS",
-        state: { to: contact.phone },
-      }}
-      onClick={() => {
-        newFunction(contact);
-      }}
-      value={contact.phone}
-    >
-      <div className="d-flex w-100 justify-content-end">
-        <small className="date-row">{contact.phone}</small>
-      </div>
-      <h6 className="mb-1">{contact.name}</h6>
-    </div>
-  );
-};
-
-export default ContactEntry;
-function newFunction(person) {
+function newFunction(person, message) {
   person.phone = person.phone.replace(/\D/ig, '');
   if (person.phone[0] === '1') {
-    person.phone = '+' + person.phone;
+    person.phone = `+${person.phone}`;
   } else {
-    person.phone = '+1' + person.phone;
+    person.phone = `+1${person.phone}`;
   }
   const number = person.phone;
   console.log(person.phone);
-  Axios.post('/api/sendSMS', { number })
+  Axios.post('/api/sendSMS', { number, message });
 }
+const ContactEntry = ({ contact }) => (
+  <div>
+    <div className="d-flex w-100 justify-content-end">
+      <small className="date-row">{contact.phone}</small>
+    </div>
+    <Popup position="right center" className="mb-1" trigger={<a className="contact-button">{contact.name}</a>}>
+      <form
+        className="message-form"
+        onSubmit={(e) => {
+          console.log(e.target.message.value);
+        newFunction(contact, e.target.message.value);
+      }}
+      >
+        <label className="message-label">Message</label>
+        <input className="message-input" name="message" />
+        <button
+          className="send-message-button"
+          to={{
+            pathname: '/sendSMS',
+            state: { to: contact.phone },
+          }}
+          value={contact.phone}
+        >Send
+        </button>
+      </form>
+    </Popup>
+  </div>
+);
+
+
+export default ContactEntry;
 
