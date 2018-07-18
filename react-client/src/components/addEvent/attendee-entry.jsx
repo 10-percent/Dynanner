0d813c92ddc1ec63dbcb6c4af02a78d4f2bfaa72
Operation Spark <operationspark@Operations-MacBook-Pro-5.local>
2018-07-18 12:34:56 -0500

    added button to remove attendee

--- react-client/src/components/addEvent/attendee-entry.jsx
+++ react-client/src/components/addEvent/attendee-entry.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const AttendeeEntry = ({ attendee, removeAttendee }) => (
   <div className="attendee-container">
-    <div className="attendee">{attendee}</div>
+    <div className="attendee"><button onClick={removeAttendee.bind(this)}>x</button>   {attendee}</div>
   </div>
 );
 
