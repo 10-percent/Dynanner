3b4ef4f73a87206db5c549170923b2f3ff9ef3c5
Operation Spark <operationspark@Operations-MacBook-Pro-5.local>
2018-07-18 12:55:51 -0500

    changed x to remove in remove button

--- react-client/src/components/addEvent/attendee-entry.jsx
+++ react-client/src/components/addEvent/attendee-entry.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const AttendeeEntry = ({ attendee, removeAttendee }) => (
   <div className="attendee-container">
-    <div className="attendee"><button onClick={removeAttendee}>x</button>   {attendee}</div>
+    <div className="attendee"><button onClick={removeAttendee}>Remove</button>   {attendee}</div>
   </div>
 );
 
